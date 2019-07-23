fcc38f5c40a75043b41a9a1a26b8f2105d728854
Brian Dam Pedersen <brian.dam.pedersen@gmail.com>
2019-07-24 00:28:10 +0200

    First installment

---
+++ part1/Output/bfs Debug/Obj/main.d
@@ -0,0 +1,5 @@
+Output/bfs\ Debug/Obj/main.o: \
+ C:\Users\brian\src\ble-from-scratch\part1\main.c \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.18/include/stdio.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.18/include/__crossworks.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.18/include/stdlib.h
---
+++ part1/Output/bfs Debug/Obj/ses_nRF_Startup.d
@@ -0,0 +1,2 @@
+Output/bfs\ Debug/Obj/ses_nRF_Startup.o: \
+ C:\Users\brian\src\ble-from-scratch\part1\nRF\Source\ses_nRF_Startup.s
